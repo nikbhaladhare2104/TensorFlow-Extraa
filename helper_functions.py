3066d947c988b12da3e75b1ec145d48d98819b98
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2023-08-03 12:04:08 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -326,3 +326,46 @@ def plot_decision_boundary(model, X, y):
   plt.ylim(yy.min(), yy.max())
 
 
+
+
+import random
+
+# Create a function for plotting a random image along with its prediction
+def plot_random_image(model, images, true_labels, classes):
+  """Picks a random image, plots it and labels it with a predicted and truth label.
+
+  Args:
+    model: a trained model (trained on data similar to what's in images).
+    images: a set of random images (in tensor form).
+    true_labels: array of ground truth labels for images.
+    classes: array of class names for images.
+  
+  Returns:
+    A plot of a random image from `images` with a predicted class label from `model`
+    as well as the truth class label from `true_labels`.
+  """ 
+  # Setup random integer
+  i = random.randint(0, len(images))
+  
+  # Create predictions and targets
+  target_image = images[i]
+  pred_probs = model.predict(target_image.reshape(1, 28, 28)) # have to reshape to get into right size for model
+  pred_label = classes[pred_probs.argmax()]
+  true_label = classes[true_labels[i]]
+
+  # Plot the target image
+  plt.imshow(target_image, cmap=plt.cm.binary)
+
+  # Change the color of the titles depending on if the prediction is right or wrong
+  if pred_label == true_label:
+    color = "green"
+  else:
+    color = "red"
+
+  # Add xlabel information (prediction/true label)
+  plt.xlabel("Pred: {} {:2.0f}% (True: {})".format(pred_label,
+                                                   100*tf.reduce_max(pred_probs),
+                                                   true_label),
+             color=color) # set the color to green or red
+
+
